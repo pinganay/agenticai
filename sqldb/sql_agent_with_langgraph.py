da717f4c543e13bae2ccf9c16f52e939c2ad3d5e
Rabisankar Sahu <rabisankarsahu@Rabisankars-MacBook-Pro.local>
2025-03-25 22:20:45 -0700

    agentic ai to use sqldb as data source

---
+++ sqldb/sql_agent_with_langgraph.py
@@ -0,0 +1,103 @@
+import sqlite3
+
+connection = sqlite3.connect("mydb.db")
+print(connection)
+
+table_creation_query="""
+CREATE TABLE IF NOT EXISTS employees (
+    emp_id INTEGER PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    hire_date TEXT NOT NULL,
+    salary REAL NOT NULL
+);
+"""
+table_creation_query2="""
+CREATE TABLE IF NOT EXISTS customers (
+    customer_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    phone TEXT
+);
+"""
+table_creation_query3="""
+CREATE TABLE IF NOT EXISTS orders (
+    order_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    customer_id INTEGER NOT NULL,
+    order_date TEXT NOT NULL,
+    amount REAL NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES customers (customer_id)
+);
+
+"""
+
+#Creating cursor to create table in database
+cursor = connection.cursor()
+
+cursor.execute(table_creation_query)
+cursor.execute(table_creation_query2)
+cursor.execute(table_creation_query3)
+
+insert_query = """
+INSERT INTO employees (emp_id, first_name, last_name, email, hire_date, salary)
+VALUES (?, ?, ?, ?, ?, ?);
+"""
+
+insert_query_customers = """
+INSERT INTO customers (customer_id, first_name, last_name, email, phone)
+VALUES (?, ?, ?, ?, ?);
+"""
+
+insert_query_orders = """
+INSERT INTO orders (order_id, customer_id, order_date, amount)
+VALUES (?, ?, ?, ?);
+"""
+employee_data = [
+    (1, "Sunny", "Savita", "sunny.sv@abc.com", "2023-06-01", 50000.00),
+    (2, "Arhun", "Meheta", "arhun.m@gmail.com", "2022-04-15", 60000.00),
+    (3, "Alice", "Johnson", "alice.johnson@jpg.com", "2021-09-30", 55000.00),
+    (4, "Bob", "Brown", "bob.brown@uio.com", "2020-01-20", 45000.00),
+    ]
+
+customers_data = [
+    (1, "John", "Doe", "john.doe@example.com", "1234567890"),
+    (2, "Jane", "Smith", "jane.smith@example.com", "9876543210"),
+    (3, "Emily", "Davis", "emily.davis@example.com", "4567891230"),
+    (4, "Michael", "Brown", "michael.brown@example.com", "7894561230"),
+]
+
+orders_data = [
+    (1, 1, "2023-12-01", 250.75),
+    (2, 2, "2023-11-20", 150.50),
+    (3, 3, "2023-11-25", 300.00),
+    (4, 4, "2023-12-02", 450.00),
+]
+
+#For testing purposes
+delete_query_employees = """
+DELETE FROM employees
+"""
+delete_query_customers = """
+DELETE FROM customers
+"""
+delete_query_orders = """
+DELETE FROM orders
+"""
+cursor.execute(delete_query_employees)
+cursor.execute(delete_query_customers)
+cursor.execute(delete_query_orders)
+
+cursor.executemany(insert_query,employee_data)
+cursor.executemany(insert_query_customers,customers_data)
+cursor.executemany(insert_query_orders,orders_data)
+
+connection.commit()
+
+#Testing the table creation
+
+cursor.execute("select * from employees;")
+
+for row in cursor.fetchall():
+    print(row)
